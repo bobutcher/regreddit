11e9156c9c896cb5141a8c223e3bd9cfe8ca4a01
Bo Butcher <cbbutcher91@gmail.com>
2016-05-23 15:23:36 -0400

    Added things

--- test/controllers/signup_controller_test.rb
+++ test/controllers/signup_controller_test.rb
@@ -3,6 +3,9 @@ require 'test_helper'
 class SignupControllerTest < ActionController::TestCase
   test "can signup" do
     get :create, user: {email: "bo@butcher.com", password: "password"}
+
     assert_redirected_to links_path
+
+    assert session[:user_id]
   end
 end
